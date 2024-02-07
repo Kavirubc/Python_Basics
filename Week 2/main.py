7f11ecf4b98b1a2f64da48479b80903603d44ffd
kaviru <hapuarachchikaviru@gmail.com>
2024-02-07 20:08:17 +0530

    Add tuples and print their length

--- Week 2/main.py
+++ Week 2/main.py
@@ -1,1 +1,15 @@
 #Python and Data structures
+
+#tuples
+
+tuple1=('Hello', 'World', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+print(tuple1[0])
+
+tuple2=("you", "are", "awesome")
+
+t = tuple1 + tuple2
+
+print(t)
+
+print(len(t))
