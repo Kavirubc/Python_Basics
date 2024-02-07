5050df80fa0d47d90d4fee9dd5ce9d8f91e1412a
kaviru <hapuarachchikaviru@gmail.com>
2024-02-07 20:24:36 +0530

    Refactor code to improve performance and readability

---
+++ Week 2/test1.py
@@ -0,0 +1,14 @@
+
+var ="Hello World!".split()
+
+print(var)
+
+
+list1=["hi", "I", "am", "awesome"]
+
+list1.extend(["you", "are", "awesome"])
+
+# list3=list1.extend(["you", "are", "awesome"])
+
+print(list1)
+# print(list3)
